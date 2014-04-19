140761a2e8e05cc232ad72e26b4eb05cd13765f9
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-04-19 18:28:59 +0100

    Add the Grid class to represent the game map.

---
+++ app/src/main/java/com/cmov/bombermanandroid/app/model/Grid.java
@@ -0,0 +1,64 @@
+package com.cmov.bombermanandroid.app.model;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.cmov.bombermanandroid.app.R;
+
+public class Grid {
+
+    private final int HEIGHT = 13;
+    private final int WIDTH = 19;
+
+    private Bitmap bomb;
+    private Bitmap bomberman;
+    private Bitmap floor;
+    private Bitmap obstacle;
+    private Bitmap robot;
+    private Bitmap wall;
+
+    private Model[][] gameMap;
+
+    public Grid(Context context){
+        this.bomb = BitmapFactory.decodeResource(context.getResources(), R.drawable.bomb);
+        this.bomberman = BitmapFactory.decodeResource(context.getResources(), R.drawable.bomberman);
+        this.floor = BitmapFactory.decodeResource(context.getResources(), R.drawable.grass);
+        this.obstacle = BitmapFactory.decodeResource(context.getResources(), R.drawable.obstacle);
+        this.robot = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        this.wall = BitmapFactory.decodeResource(context.getResources(), R.drawable.wall);
+
+        this.gameMap = new Model[HEIGHT][WIDTH];
+    }
+
+
+    public void addBomb(int x, int y, int spread, int damage, int depth){
+        Bomb bomb = new Bomb(this.bomb, x, y, spread, damage, depth);
+        this.gameMap[x][y] = bomb;
+    }
+
+    public void addBomberman(int x, int y, int lives, float speed, boolean isDead){
+        Bomberman bomberman = new Bomberman(this.bomberman, x, y, lives, speed, isDead);
+        this.gameMap[x][y] = bomberman;
+    }
+
+    public void addFloor(int x, int y){
+        Floor floor = new Floor(this.floor, x, y);
+        this.gameMap[x][y] = floor;
+    }
+
+    public void addObstacle(int x, int y, int hitPoints, boolean isVisible){
+        Obstacle obstacle = new Obstacle(this.obstacle, x, y, hitPoints, isVisible);
+        this.gameMap[x][y] = obstacle;
+    }
+
+    public void addRobot(int x, int y, float speed, boolean isDead){
+        Robot robot = new Robot(this.robot, x, y, speed, isDead);
+        this.gameMap[x][y] = robot;
+    }
+
+    public void addWall(int x, int y){
+        Wall wall = new Wall(this.wall, x, y);
+        this.gameMap[x][y] = wall;
+    }
+}
