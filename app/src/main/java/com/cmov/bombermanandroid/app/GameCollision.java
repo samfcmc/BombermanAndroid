93614faa563cd1d8bba6477dcd5f57b66f53b7f3
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-04-26 13:03:01 +0100

    Update the GameCollision module.

--- app/src/main/java/com/cmov/bombermanandroid/app/GameCollision.java
+++ app/src/main/java/com/cmov/bombermanandroid/app/GameCollision.java
@@ -1,14 +1,24 @@
 package com.cmov.bombermanandroid.app;
 
 import com.cmov.bombermanandroid.app.model.Grid;
+import com.cmov.bombermanandroid.app.model.Model;
 import com.cmov.bombermanandroid.app.model.Movable;
 
 public class GameCollision {
 
     private static Grid grid;
 
+    public static void setGrid(Grid grid) {
+        GameCollision.grid = grid;
+    }
+
     public static boolean checkRightCollision(Movable character){
-        return false;
+        Model model = grid.getModel(character.getX() + 1, character.getY());
+        if(model == null){
+            grid.updateGridAfterMovement(character.getX(), character.getY(), character.getX() + 1, character.getY());
+            return false;
+        }
+        else return model.isCollidable();
     }
 
     public static boolean checkDeadlyRightCollison(Movable character){
@@ -16,7 +26,12 @@ public class GameCollision {
     }
 
     public static boolean checkLeftCollision(Movable character){
-        return false;
+        Model model = grid.getModel(character.getX() - 1, character.getY());
+        if(model == null){
+            grid.updateGridAfterMovement(character.getX(), character.getY(), character.getX() + 1, character.getY());
+            return false;
+        }
+        else return model.isCollidable();
     }
 
     public static boolean checkDeadlyLeftCollision(Movable character){
@@ -24,7 +39,12 @@ public class GameCollision {
     }
 
     public static boolean checkDownCollision(Movable character){
-        return false;
+        Model model = grid.getModel(character.getX(), character.getY() - 1);
+        if(model == null){
+            grid.updateGridAfterMovement(character.getX(), character.getY(), character.getX(), character.getY() - 1);
+            return false;
+        }
+        else return model.isCollidable();
     }
 
     public static boolean checkDeadlyDownCollision(Movable character){
@@ -32,7 +52,12 @@ public class GameCollision {
     }
 
     public static boolean checkUpCollision(Movable character){
-        return false;
+        Model model = grid.getModel(character.getX(), character.getY() + 1);
+        if(model == null){
+            grid.updateGridAfterMovement(character.getX(), character.getY(), character.getX(), character.getY() + 1);
+            return false;
+        }
+        else return model.isCollidable();
     }
 
     public static boolean checkDeadlyUpCollision(Movable character){
