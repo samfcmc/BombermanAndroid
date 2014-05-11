c33edda1c9f8a8aacd7d87c3d289ee9e79699f77
Samuel Coelho <samuelfcmc@gmail.com>
2014-05-11 14:43:13 +0100

    Created the class MultiplayerManager. Start implementing #24

---
+++ app/src/main/java/com/cmov/bombermanandroid/app/multiplayer/MultiplayerManager.java
@@ -0,0 +1,8 @@
+package com.cmov.bombermanandroid.app.multiplayer;
+
+/**
+ * MultiplayerManager: This class handles all the multiplayer behavior and
+ * act as a wrapper around the WifiDirect API or any simulator of it
+ */
+public class MultiplayerManager {
+}
